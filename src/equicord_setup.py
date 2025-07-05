a80d50fb17f7f72536b8e80863d7b12bf48f9627
ExtbhiteEAS <extbhite@ya.ru>
2025-07-05 17:01:01 +0300

    feat(build): Update
    - Added Python scripts, which is included CSP fix for Vencord plugin fakeProfile
    - Changed workflow custom build of Equicord: now setup with Python script

---
+++ src/equicord_setup.py
@@ -0,0 +1,63 @@
+import subprocess
+import os
+
+class NeededStrings:
+    temp_upstream_path: str = 'temp-upstream/'
+    equicord_url: str = 'https://github.com/Equicord/Equicord.git'
+    equicord_path: str = 'main-repo/Equicord/'
+    equicord_temp: str = 'temp-equicord/'
+
+def setup():
+    print('Setup Equicord directory')
+    subprocess.run(['mkdir', '-p', f'{NeededStrings.equicord_path}'])
+
+    subprocess.run([
+        'git', 'clone',
+        '--depth 1',
+        '--single-branch', '--branch main', f'{NeededStrings.equicord_url}',
+        f'{NeededStrings.equicord_temp}'
+    ])
+
+    subprocess.run([
+        'rsync', '-a', '--delete',
+        '--exclude=\'.git\'', '--exclude=\'node_modules\'',
+        f'{NeededStrings.equicord_temp}', f'{NeededStrings.equicord_path}'
+    ])
+
+    subprocess.run(['rm', '-rf', f'{NeededStrings.equicord_temp}'])
+
+    print('Update Equicord from upstream')
+    subprocess.run([
+        'git', 'clone',
+        '--depth 1',
+        '--single-branch', '--branch main',
+        f'{NeededStrings.equicord_url}'
+    ], f'{NeededStrings.temp_upstream_path}')
+
+    subprocess.run([
+        'rsync', '-a', '--delete',
+        '--exclude=\'.git\'', '--exclude=\'node_modules\'',
+        f'{NeededStrings.temp_upstream_path}', f'{NeededStrings.equicord_path}'
+    ])
+
+    subprocess.run(['rm', '-rf', f'{NeededStrings.temp_upstream_path}'])
+
+    print('Modify .gitignore for dist and executables')
+    if os.path.exists('main-repo/Equicord/.gitignore'):
+        subprocess.run([
+            'sed', '-i',
+            '-e', '\'/dist/d\'',
+            '-e', '\'/\.exe/d\'',
+            '-e', '\'/vencord_installer/d\'',
+            '.gitignore'
+        ], cwd = 'main-repo/Equicord')
+
+        subprocess.run(['echo "# Dist and executable files are intentionally tracked in this fork" >> .gitignore'])
+    else:
+        print('No .gitignore file found')
+
+    print('Install dependencies')
+    subprocess.run(['npm', 'i', '-g', 'pnpm'], cwd = 'main-repo/Equicord')
+    subprocess.run(['pnpm', 'i' '--no-frozen-lockfile'], cwd = 'main-repo/Equicord') # WARN: --no-frozen-lockfile is very important, do not erase from subproccess command.
+
+setup()
