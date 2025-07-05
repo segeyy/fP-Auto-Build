dd5ced93c05ea071e972afd4deca63d946e20597
Extbhite <extbhite@ya.ru>
2025-07-05 19:46:05 +0300

    Delete Equicord directory

--- Equicord/packages/vencord-types/node_modules/discord-types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * as Other from './other';
-export * as Stores from './stores';
-export * as General from './general';
-export * as Actions from './actions';
-export * as Components from './components';
